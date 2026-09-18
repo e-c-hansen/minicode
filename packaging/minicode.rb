378651d3b3173d3c5a1c6e61b03a1fecd0c70cc9
echansen <eric.calvin.hansen@gmail.com>
2026-09-18 14:57:04 -0400

    Release 1.2.0

--- packaging/minicode.rb
+++ packaging/minicode.rb
@@ -5,8 +5,8 @@
 # This is a reference copy; the live cask lives in the homebrew-tap repo and is
 # updated by scripts/release.sh. To cut a new version: scripts/release.sh 1.2.0
 cask "minicode" do
-  version "1.1.0"
-  sha256 "494de1eff9885991b69fc89fdfb672555712fdee97fd62c287e161397de13360"
+  version "1.2.0"
+  sha256 "3e59bcc2289af5142b31c75a06669c6214f9081807de14561071b81cd185b870"
 
   url "https://github.com/e-c-hansen/homebrew-tap/releases/download/v#{version}/MiniCode.zip"
   name "MiniCode"
