f90a91953b2cc2363838b0265b20add9a08bd618
echansen <eric.calvin.hansen@gmail.com>
2026-09-26 04:41:26 -0400

    Release 1.4.4

--- packaging/minicode.rb
+++ packaging/minicode.rb
@@ -6,8 +6,8 @@
 # it and copies it into the homebrew-tap repo, which holds nothing else, so edit
 # it here and never there. To cut a new version: scripts/release.sh 1.2.1
 cask "minicode" do
-  version "1.4.3"
-  sha256 "d0d60e066f790774d74e39d287f783a3656321b0fc46d2136fc7f6fd65f2495e"
+  version "1.4.4"
+  sha256 "44fa1566dc9ec8dcf6b854708e77fb8a18002fdf0468762e6c71167a07139372"
 
   url "https://github.com/e-c-hansen/minicode/releases/download/v#{version}/MiniCode.zip"
   name "MiniCode"
